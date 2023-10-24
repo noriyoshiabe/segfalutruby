7e2c36a980f04ffe42af4439baad916add83ce49
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2023-10-24 14:07:28 +0900

    Force segmentation fault

--- ext/segfalutruby/segfalutruby.c
+++ ext/segfalutruby/segfalutruby.c
@@ -2,8 +2,15 @@
 
 VALUE rb_mSegfalutruby;
 
+static VALUE exec(VALUE self)
+{
+    sprintf((char *)0, "oops!");
+    return 0;
+}
+
 void
 Init_segfalutruby(void)
 {
   rb_mSegfalutruby = rb_define_module("Segfalutruby");
+  rb_define_singleton_method(rb_mSegfalutruby, "exec", exec, 0);
 }
